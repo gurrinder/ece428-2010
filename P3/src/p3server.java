79abf5461d114bdf72e43bd552ed4b2309b528a8
tejas.mandke <tejas.mandke@7ea294de-d8ea-d36d-639b-8cf55acd01cd>
2010-07-23 23:51:40 +0000

    attempt

--- P3/src/p3server.java
+++ P3/src/p3server.java
@@ -110,6 +110,13 @@ class Decoder
 		int divSize = (int) (comb(128, this.numZero)/n);
 		int start = 0;
 		int end = 128;
+		int numZ = this.numZero;
+		int useOnes = 0;
+		if (numZ>63)
+		{
+			numZ = 128-numZ;
+			useOnes = 1;
+		}
 		for (int i = 0; i < n-1; i++)
 		{
 			int subSum = 0;
@@ -118,7 +125,7 @@ class Decoder
 			{
 				lastSubSum = subSum;
 				end--;
-				subSum += comb(end, this.numZero-1);
+				subSum += comb(end, numZ-1);
 			}
 			if (divSize - lastSubSum < subSum - divSize)
 			{
@@ -126,11 +133,11 @@ class Decoder
 				end++;
 			}
 			assert start <= (128-end-1);
-			workers.add(new DecoderWorker(this.numZero, start, 128-end-1));
+			workers.add(new DecoderWorker(numZ, start, 128-end-1, useOnes));
 			System.out.printf("%3d-%3d : Worst Case takes 4s/1,000,000 keys time=%d\n", start, 128-end-1, subSum*4/1000000);
 			start = 128-end;
 		}
-		workers.add(new DecoderWorker(this.numZero, start, 128-this.numZero));
+		workers.add(new DecoderWorker(numZ, start, 128-numZ, useOnes));
 		System.out.printf("%3d-%3d : Worst Case takes 4s/1,000,000 keys time=%d\n", start, 128-this.numZero, (int)(comb(end, this.numZero)*4/1000000));
 	}
 	// calculates nCr
@@ -245,12 +252,14 @@ class DecoderWorker implements Runnable
 	private boolean keyFoundByAnother;
 	private Thread myThread;
 	public byte[] plainText;
+	private int useOnes;
 
-	DecoderWorker(int n, int start, int end) throws NoSuchAlgorithmException, NoSuchPaddingException
+	DecoderWorker(int n, int start, int end, int useOnes) throws NoSuchAlgorithmException, NoSuchPaddingException
 	{
 		this.numZero = n;
 		this.start = start;
 		this.end = end;
+		this.useOnes = useOnes;
 		cipher = Cipher.getInstance("AES/CBC/NoPadding");
 		//System.out.printf("%3d-%3d : Initialized\n", this.start, this.end);
 	}
@@ -333,8 +342,8 @@ class DecoderWorker implements Runnable
 		for (int i = start; i <= end; i++)
 		{
 			if (i != start)
-				setBit(newkeyval, i-1, 1);
-			setBit(newkeyval, i, 0);
+				setBit(newkeyval, i-1, Math.abs(useOnes-1));
+			setBit(newkeyval, i, useOnes);
 			plainText = computeKeyAndDecode(msg, i+1, 128-this.numZero+z+1, z+1, newkeyval);
 			if (this.keyFoundByAnother || plainText != null)
 			{
@@ -381,11 +390,11 @@ class DecoderWorker implements Runnable
 	@Override
 	public void run()
 	{
-		byte[] keyval = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-				-1, -1, -1 };
+		byte[][] keyval = {{ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+				-1, -1, -1 }, {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
 		try
 		{
-			this.plainText = computeKeyAndDecode(msg, this.start, this.end, 0, keyval);
+			this.plainText = computeKeyAndDecode(msg, this.start, this.end, 0, keyval[useOnes]);
 		} catch (InvalidKeyException e)
 		{
 			e.printStackTrace();
